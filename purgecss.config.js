a026f23d2bce5c3a609b2b6817b68220a9b117a5
esther57-web <kamardine.ossete@gmail.com>
2024-05-16 18:19:22 +0200

    unused CSS to fix

---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['index.html', './assets/*.js', './assets/bootstrap/*.js'],
+    css: ['./assets/bootstrap/bootstrap.min1.css'],
+    keyframes: true,
+    output: '.',
+    rejected: true,
+    variables: true,
+}
